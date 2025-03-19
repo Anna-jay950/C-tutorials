116c8603ecc98956314f67f8a70368b361e0ef51
Annie Gamadi <gamadiannie@gmail.com>
2025-03-19 11:42:06 +0000

    adding two numbers

---
+++ add.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+    cout << "Enter two numbers";
+    cin >> x;
+    cin >> y;
+
+    z = x+y;
+    cout << z;
+
+    return 0;
+}
